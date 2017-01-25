741cc6c5501bac22488b2b8249d5601d6396d182
Nathan Hoad <nathan@nathanhoad.net>
2017-01-25 13:35:03 +1000

    Removed missing plugin reference

--- templates/webpack.config.js
+++ templates/webpack.config.js
@@ -5,7 +5,6 @@ const Webpack = require('webpack');
 const AutoPrefixer = require('autoprefixer');
 const HtmlPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const BabiliPlugin = require("babili-webpack-plugin");
 
 const APP_ROOT = require('app-root-path').toString();
 const PRODUCTION = (process.env.NODE_ENV == 'production');
