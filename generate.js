00990d5e7e0b7e025069fa2e21aecdde873ef4ba
Nathan Hoad <nathan@nathanhoad.net>
2017-02-16 14:33:38 +1000

    Fixed action generating with pluralized dashed names

--- generate.js
+++ generate.js
@@ -438,6 +438,7 @@ module.exports = Prerequisites;`;
             saveTemplate('actions.js', {
                 plural_constant: Inflect.underscore(Inflect.pluralize(name)).toUpperCase(),
                 plural_lowercase: Inflect.underscore(Inflect.pluralize(name)).toLowerCase(),
+                plural_lowercase_dashed: Inflect.dasherize(Inflect.pluralize(name)).toLowerCase(),
                 plural_class: Inflect.camelize(Inflect.pluralize(Inflect.underscore(name))),
                 single_constant: Inflect.underscore(Inflect.singularize(name)).toUpperCase(),
                 single_lowercase: Inflect.underscore(Inflect.singularize(name)).toLowerCase(),
